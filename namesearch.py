6cf4a12af55ffd79d6fec9d6b4db3d5c05e18423
DT <dtsui918@yahoo.com>
2014-10-07 13:40:36 -0400

    template

--- namesearch.py
+++ namesearch.py
@@ -1,2 +1,15 @@
 import google
 
+def search(q):
+    #takes question q, returns list of urls
+    print ""
+
+def parse_urls(urlsList):
+    #takes list of urls, returns only text
+    #beautiful soup
+
+def name_occurrence(text):
+    #finds names in text
+
+def analyse_list(namesList):
+    #takes list of names, returns analysis + answer to question
