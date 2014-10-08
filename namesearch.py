b7a554070977f9ae8ca81103ab0a3b73a3c1c783
Tianshi Wang <twang.123@live.com>
2014-10-08 13:11:28 -0400

    finished parse_urls

--- namesearch.py
+++ namesearch.py
@@ -1,4 +1,5 @@
 import google
+from bs4 import BeautifulSoup
 
 def search(q):
     #takes question q, returns list of urls
@@ -7,9 +8,18 @@ def search(q):
 def parse_urls(urlsList):
     #takes list of urls, returns only text
     #beautiful soup
-
-def name_occurrence(text):
+    html = ""
+    for u in urlsList:
+        content = google.get_page(u)
+        soup = BeautifulSoup(content)
+        html += soup.get_text()
+    return html
+ 
+#def name_occurrence(text):
     #finds names in text
 
-def analyse_list(namesList):
+#def analyse_list(namesList):
     #takes list of names, returns analysis + answer to question
+
+
+parse_urls(["http://www.pythonforbeginners.com/python-on-the-web/beautifulsoup-4-python/"])
