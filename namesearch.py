b89334a924a6efba7eb7d0b407ad7a584ae61e83
DT <dtsui918@yahoo.com>
2014-10-08 13:05:52 -0400

    search function

--- namesearch.py
+++ namesearch.py
@@ -2,14 +2,24 @@ import google
 
 def search(q):
     #takes question q, returns list of urls
-    print ""
+    urlsList = []
+    for url in google.search(q,tld='com',lang='en',num=10,start=0,stop=20):
+        urlsList.append(url)
+    return urlsList
 
 def parse_urls(urlsList):
     #takes list of urls, returns only text
     #beautiful soup
+    print ""
 
 def name_occurrence(text):
     #finds names in text
+    print ""
 
 def analyse_list(namesList):
     #takes list of names, returns analysis + answer to question
+    print ""
+
+if __name__ == "__main__":
+    urlsList = search("Who is the President of the United States?")
+    parse_urls(urlsList)
