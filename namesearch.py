fd5e7ae53c409bf1aabd2ff899283e43e63e0e1e
d-tsui <dtsui918@yahoo.com>
2014-10-13 22:03:40 -0400

    dummy values for now

--- namesearch.py
+++ namesearch.py
@@ -1,4 +1,5 @@
 import google
+import re
 from bs4 import BeautifulSoup
 
 def search(q):
@@ -6,6 +7,7 @@ def search(q):
     urlsList = []
     for url in google.search(q,tld='com',lang='en',num=10,start=0,stop=20):
         urlsList.append(url)
+    print "Generated URL list!"
     return urlsList
 
 def parse_urls(urlsList):
@@ -16,19 +18,42 @@ def parse_urls(urlsList):
         content = google.get_page(u)
         soup = BeautifulSoup(content)
         html += soup.get_text()
+    print "Parsed HTML code!"
     return html
 
 def name_occurrence(text):
-    #finds names in text
-    print ""
+    names = []
+    with open("names_list.txt") as f:
+        for line in f:
+            names.append(line.split()[0].capitalize())
+
+    extras = ["Mister", "Mr.", "Mrs.", "Ms.", "Master", "Dr.", "Doctor", "President"]
+    for item in extras:
+        names.append(item)
+
+    #test prints
+    #print(text[:100])
+    #print names[:10]
+
+    search = ""
+    results = []
+    for item in names:
+        #print item
+        search = r"(([A-Z][a-z-]+){1,2}\s){2,3}"
+        L = re.findall(search,  text)
+        if (L != []):
+            results.append(L[0]);
+    print "Parsed names!"
+    print results
 
 def analyse_list(namesList):
     #takes list of names, returns analysis + answer to question
-    print ""
-    
+    results = {"First Result":5000,"Second Result":2000,"Third Result":1000}
+    return results
+
 def get_analysis(q):
     return analyze_list(name_occurence(parse_urls(search(q))))
 
 if __name__ == "__main__":
     urlsList = search("Who is the President of the United States?")
-    parse_urls(urlsList)
+    name_occurrence(parse_urls(urlsList))
